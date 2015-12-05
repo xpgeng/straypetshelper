c18eb2b4dbce4c0405f3c1813e2d4b90f4d2534f
xpgeng <xpgeng@126.com>
2015-12-05 22:43:41 +0800

    try to use fabric and add src as source files folder

--- fabfile.py
+++ fabfile.py
@@ -3,24 +3,12 @@ from fabric.api import env, run, local, settings
 from datetime import datetime
 
 
-
-
-def gitmaster(m='Commit something to master...'):
-	'''gitmaster:m = "COMMIT LOGGING"\t(default as 'Commit something...')
-	'''	
-	local('pwd'		    
-		    '&& git add .'
-		    '&& git commit -am "{msg}"'
-		    '&& git push origin master'
-		    '&& date'.format(msg = m)
-        )
-
-def gitdev(m='Commit something to dev...'):
+def gitbranch(m='Commit something to dev...'):
 	'''gitdev:m = "COMMIT LOGGING"\t(default as 'Commit something to dev...')
 	'''	
 	local('pwd'		    
-		    '&& git add .'
+		    '&& git add src'
 		    '&& git commit -am "{msg}"'
-		    '&& git push origin dev'
+		    '&& git push origin feature/web-framework'
 		    '&& date'.format(msg = m)
         )
