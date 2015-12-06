aceffb3e145e6f67160c0feea540aed71a24e1b5
xpgeng <xpgeng@126.com>
2015-12-06 18:22:58 +0800

    debug-- if the photoname is Chinese character

--- src/main.py
+++ src/main.py
@@ -6,43 +6,57 @@ Author: Shenlang
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
- 
-import sae, sae.kvdb
-from flask import Flask, request, render_template
-from time import strftime, localtime
+
 import os
+import sae, sae.kvdb
+import time
+from flask import Flask, request, render_template, url_for, send_from_directory
+from time import strftime, localtime
 from werkzeug import secure_filename
 
-app = Flask(__name__)
 
-UPLOAD_FOLDER = '/path/to/the/uploads'
+ROOT = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = ROOT+'/uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
+
 def allowed_file(filename):
-	return '.' in filename and \
-		filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+	return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+def check_filename(filename):
+    if filename in ALLOWED_EXTENSIONS:
+        filename_new = "%s.%s" % (int(time.time()), filename)
+        return filename_new
+    else:
+        return filename
+
+
 
 @app.route('/')
 def submit_pet():
-	return render_template("index.html")
+    return render_template("index.html")
 
 @app.route('/', methods=['POST'])
 def check_pet():
-	pet_title = request.form['pet-title']
-	species = request.form['species']
-	location = request.form['location']
-	tel = request.form['tel']
-	supplement = request.form['supplement']
-	
-	pet_photo = request.files['petphoto']
+    pet_title = request.form['pet-title']
+    species = request.form['species']
+    location = request.form['location']
+    tel = request.form['tel']
+    supplement = request.form['supplement']
+    pet_photo = request.files['petphoto']
 
-	if pet_photo and allowed_file(pet_photo.filename):
-		filename = secure_filename(pet_photo.filename)
-		pet_photo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    if pet_photo and allowed_file(pet_photo.filename):
+        filename = secure_filename(pet_photo.filename)
+        renew_filename = check_filename(filename)
+        print renew_filename
+        pet_photo.save(os.path.join(app.config['UPLOAD_FOLDER'], renew_filename))
 
-	return render_template("check.html",pet_title = pet_title,
-		    species=species, location=location, tel=tel, supplement=supplement)
+    return render_template("check.html", pet_title=pet_title,
+            species=species, location=location, tel=tel, supplement=supplement)
 
 
 if  __name__ == "__main__":
