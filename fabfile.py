5feac1d4be379a620e484e74acca72aa9a2c2873
Huijuan Nan <huijuannan.p@gmail.com>
2015-12-11 11:45:59 +0800

    test fab

--- fabfile.py
+++ fabfile.py
@@ -2,28 +2,19 @@
 from fabric.api import env, run, local, settings
 from datetime import datetime
 
-
-def gitbranch(m='Commit something to branch...'):
-	'''gitbranch:m = "COMMIT LOGGING"\t(default as 'Commit something to dev...')
-	'''	
-	local('pwd'		    
-		    '&& git add src'
-		    '&& git commit -am "{msg}"'
-		    '&& git push origin feature/web-framework'
-		    '&& date'.format(msg = m)
-        )
-
-
-def gitmaster(f='src', m='Commit something to master...', b='fix-html'):
+def gitmaster(f='src', m='Commit something to master...', b = 'fix-html'):
 	'''gitmaster:f = "ADD FILENAMES"\t(default as 'src && README.md'), 
 	          m = "COMMIT LOGGING"\t(default as 'Commit something to master...')
 	          b = "Branch name"\t(default as 'fix-html')
 	'''	
-	local('pwd'		    
-		    '&& git add {filename}'
-		    '&& git commit -am "{msg}"'
-		    '&& git push origin "{branch}"'
-		    '&& date'
-		    '&& touch worklog.md'
-		    '"- {msg}" >> worklog.md'.format(filename= f , msg = m , branch = b)
+	local('pwd '
+		'&& date '
+		'&& touch worklog.md '
+		'&& git add {file} '
+		'&& git commit -am "{msg}" '
+		'&& git push origin fix-html '
+		'&& echo "- {msg}" >> worklog.md'.format(msg = m, file=f, branch=b)
         )
+
+def deploysae():
+	local('')
